b119224474ebcc1f19af1e66e125e592472bbf5c
waa-4 <gregorycheney209@gmail.com>
2026-03-03 16:17:31 -0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+// data.js
+// Add new drawings here. Easiest editing: copy/paste one block.
+
+window.MUSEUM_CONFIG = {
+  title: "Art I guess",
+  subtitle: "good pictures. 🗿",
+};
+
+window.MUSEUM_ITEMS = [
+  {
+    id: "o1",
+    title: "Cloud Thingy",
+    description: "Simple, but good.",
+    image: "./assets/o1.png",
+    date: "2026-03-03",
+    tags: ["Scene", "Cartoon"],
+  },
+  {
+    id: "art-002",
+    title: "Galaxy Wallpaper",
+    description: ":)",
+    image: "./assets/02.png",
+    date: "2026-03-03",
+    tags: ["Space", "Wallpaper"],
+  },
+  {
+    id: "art-003",
+    title: "Everything Explosion",
+    description: "Like a big bang, but not.. so deadly",
+    image: "./assets/03.png",
+    date: "2026-03-02",
+    tags: ["Space"],
+  },
+];
